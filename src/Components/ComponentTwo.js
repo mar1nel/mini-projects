2d16d306d88f67cd75b74a97ba0903534c9cba26
mar1nel <vicu-marin.ababii@student.upt.ro>
2023-05-06 23:44:18 +0300

    random gif api

--- src/Components/ComponentTwo.js
+++ src/Components/ComponentTwo.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import '../App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function ComponentTwo() {
+    const [gifUrl, setGifUrl] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const [open, setOpen] = React.useState(false);
+    const fetchRandomGif = async () => {
+        setIsLoading(true);
+        try {
+            const { data } = await axios.get('https://api.giphy.com/v1/gifs/random', {
+                params: {
+                    api_key: 'zkuzymN4PC3Z5ZLHVNsKtRpZx4e4R4bA',
+                    tag: 'cat',
+                    rating: 'r',
+                },
+            });
+            setGifUrl(data.data.images.original.url);
+        } catch (error) {
+            setErrorMessage('Failed');
+        }
+        setIsLoading(false);
+    };
 
-    return(
+    const handleClose = () => {
+        setGifUrl('');
+    };
+
+    useEffect(() => {
+        fetchRandomGif();
+    }, []);
+
+    return (
         <>
             <br />
-            <button className="open-modal" onClick={() => setOpen(!open)}>
-                Open &gt;w&lt;
+            <button className="open-modal" onClick={fetchRandomGif}>
+                {isLoading ? 'Loading...' : 'Open >w<'}
             </button>
-            {
-                open && (
-                    <div className="overlay">
-                        <div className="modal">
-                            <img src="https://media.tenor.com/2-VH7ZH6rxQAAAAC/cat-tongue.gif" alt="gif"
-                                 style={{ width: '300px', height: '300px' }} />
-                        </div>
+            {errorMessage && <div>{errorMessage}</div>}
+            {gifUrl && (
+                <div className="overlay">
+                    <div className="modal">
+                        <button onClick={handleClose}>X</button>
+                        <img
+                            src={gifUrl}
+                            alt="Random gif"
+                            style={{ width: '300px', height: '300px' }}
+                        />
                     </div>
-                )
-            }
+                </div>
+            )}
         </>
     );
 }
